cadbbb2c4f1844b7116a1ac024f5d0c25de2c6eb
ayoub hadi <ayoub.hadi.stage@worldaudit.ma>
2021-10-01 18:47:05 +0100

    fix error not found

--- src/main/java/com/example/testjenkins/demo/DemoController.java
+++ src/main/java/com/example/testjenkins/demo/DemoController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.testjenkins.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,5 +9,10 @@ public class DemoController {
     @GetMapping(path = "/home")
     public String home(){
         return "home";
+    }
+
+    @GetMapping(path = "/test")
+    public String test(){
+        return "test";
     }
 }
